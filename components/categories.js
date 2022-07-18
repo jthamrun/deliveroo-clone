5e7d90f0b8bce1f649879b094c4bc83a932e722e
Jonathan Thamrun <jthm@jonathans-mini.lan>
2022-07-18 01:42:22 -0700

    Home Screens Update

--- components/categories.js
+++ components/categories.js
@@ -1,8 +1,25 @@
 import { View, Text, ScrollView } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CategoryCard from "./CategoryCard";
+import sanityClient, { urlFor } from "../sanity";
 
 const Categories = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        sanityClient
+            .fetch(
+                `
+        *[_type == "category"]
+        `
+            )
+            .then((data) => {
+                setCategories(data);
+            });
+    }, []);
+
+    // console.log(categories);
+
     return (
         <ScrollView
             horizontal
@@ -10,30 +27,13 @@ const Categories = () => {
             contentContainerStyle={{ paddingHorizontal: 15, paddingTop: 10 }}
         >
             {/* CategoryCard */}
-            <CategoryCard
-                imgUrl="https://links.papareact.com/gn7"
-                title="Testing 1"
-            />
-            <CategoryCard
-                imgUrl="https://links.papareact.com/gn7"
-                title="Testing 2"
-            />
-            <CategoryCard
-                imgUrl="https://links.papareact.com/gn7"
-                title="Testing 3"
-            />
-            <CategoryCard
-                imgUrl="https://links.papareact.com/gn7"
-                title="Testing 3"
-            />
-            <CategoryCard
-                imgUrl="https://links.papareact.com/gn7"
-                title="Testing 3"
-            />
-            <CategoryCard
-                imgUrl="https://links.papareact.com/gn7"
-                title="Testing 3"
-            />
+            {categories.map((category) => (
+                <CategoryCard
+                    key={category._id}
+                    imgUrl={urlFor(category.image).width(200).url()}
+                    title={category.name}
+                />
+            ))}
         </ScrollView>
     );
 };
--- sanity.js
+++ sanity.js
@@ -1,8 +1,12 @@
 import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
+const projectId = process.env.PROJECT_ID;
+
+//`${projectId}`
+
 const client = sanityClient({
-    projectId: process.env.PROJECT_ID,
+    projectId: "k2tgqajf",
     dataset: "production",
     useCdn: true,
     apiVersion: "2021-10-21",
