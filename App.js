f0eb46baa37ac0918039871a26d77bedb3e143f0
Jonathan Thamrun <jthm@jonathans-mini.lan>
2022-07-20 01:43:08 -0700

    Update

--- App.js
+++ App.js
@@ -4,17 +4,22 @@ import { TailwindProvider } from "tailwindcss-react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
+import RestaurantScreen from "./screens/RestaurantScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <TailwindProvider>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-        </Stack.Navigator>
-      </TailwindProvider>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <TailwindProvider>
+                <Stack.Navigator>
+                    <Stack.Screen name="Home" component={HomeScreen} />
+                    <Stack.Screen
+                        name="Restaurant"
+                        component={RestaurantScreen}
+                    />
+                </Stack.Navigator>
+            </TailwindProvider>
+        </NavigationContainer>
+    );
 }
